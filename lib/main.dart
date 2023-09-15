ce264c77fd84c535f74e100131a2ef103c57f559
ryansyaaw <hazama.ryan1@gmail.com>
2023-09-15 14:47:48 +0700

    week2.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); 
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData( 
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 162, 255)),
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 162, 255)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Ryan Syaputra AW'),
@@ -23,6 +26,7 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   void _incrementCounter() {
@@ -30,16 +34,19 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+
   void _decrementCounter() {
     setState(() {
       _counter--;
     });
   }
-    void _resetCounter() {
+
+  void _resetCounter() {
     setState(() {
       _counter = 0;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,19 +54,33 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(  
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Ryan Syaputra A.W',
-            ),
-            const Text(
-              '2141720089',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+      body: Center(
+        child: Stack(
+          children: [
+            const Image(image: NetworkImage('https://plus.unsplash.com/premium_photo-1694618625193-3d056dcd1ca7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60')),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Ryan Syaputra A.W',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  const Text(
+                    '2141720089',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  Text(
+                    '$_counter',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 50
+                    ),
+                    
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -70,20 +91,20 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             FloatingActionButton(
-                  onPressed: _incrementCounter,
-                  tooltip: 'increment',
-                  child: const Icon(Icons.add),
-                ),
-                FloatingActionButton(
-                  onPressed: _decrementCounter,
-                  tooltip: 'decrement',
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: _resetCounter,
-                  tooltip: 'reset',
-                  child: const Icon(Icons.refresh),
-                ), 
+              onPressed: _incrementCounter,
+              tooltip: 'increment',
+              child: const Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: _decrementCounter,
+              tooltip: 'decrement',
+              child: const Icon(Icons.remove),
+            ),
+            FloatingActionButton(
+              onPressed: _resetCounter,
+              tooltip: 'reset',
+              child: const Icon(Icons.refresh),
+            ),
           ],
         ),
       ),
